4acf58c482b82eb722e9633c4f9b2dbe879702b4
Santiago Albarracin <sa_albarracin@hotmail.com>
2025-06-04 22:30:06 -0700

    Update styles in all pages

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,24 +1,25 @@
 const toggleSwitch = document.querySelector('#checkbox');
-const themeLabel = document.querySelector('#theme-label');
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = localStorage.getItem('theme');
 
+// Set dark mode as default
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
+    if (currentTheme === 'light') {
         toggleSwitch.checked = true;
-        themeLabel.textContent = 'Light Mode'; // Update button text for dark mode
     }
+} else {
+    // Dark mode is default
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        themeLabel.textContent = 'Light Mode'; // Change to "Light Mode" in dark mode
-    } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-        themeLabel.textContent = 'Dark Mode'; // Change to "Dark Mode" in light mode
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
 }
 
